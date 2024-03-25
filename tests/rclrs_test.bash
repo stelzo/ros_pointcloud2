e231047056d21ee78a3049c0922097c83acd3ee9
stelzo <csieh@uos.de>
2024-03-25 13:04:15 +0100

    iron rclrs

--- tests/rclrs_test.bash
+++ tests/rclrs_test.bash
@@ -15,11 +15,6 @@ if [ -e "/opt/ros/humble/setup.bash" ]; then
     . "/ros2_rust_build/install/local_setup.bash"
 fi
 
-if [ -e "/opt/ros/galactic/setup.bash" ]; then
-    . "/opt/ros/galactic/setup.bash"
-    . "/ros2_rust_build/install/local_setup.bash"
-fi
-
 cd /ros2_rust_build/src/ros_pointcloud2_tests/ || exit
 
 "$@"
