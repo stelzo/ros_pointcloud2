15a1db79d641233cebb70e728b46248d63b3e759
Christopher Sieh <stelzo@steado.de>
2024-10-18 10:31:51 +0200

    source jazzy

--- rpcl2/tests/rclrs_test.bash
+++ rpcl2/tests/rclrs_test.bash
@@ -5,16 +5,19 @@
 # run rustup to test with latest rust version
 rustup update
 
+if [ -e "/opt/ros/jazzy/setup.bash" ]; then
+    . "/opt/ros/jazzy/setup.bash"
+fi
+
 if [ -e "/opt/ros/iron/setup.bash" ]; then
     . "/opt/ros/iron/setup.bash"
-    . "/ros2_rust_build/install/local_setup.bash"
 fi
 
 if [ -e "/opt/ros/humble/setup.bash" ]; then
     . "/opt/ros/humble/setup.bash"
-    . "/ros2_rust_build/install/local_setup.bash"
 fi
 
+. "/ros2_rust_build/install/local_setup.bash"
 cd /ros2_rust_build/src/ros_pointcloud2_tests/ || exit
 
 "$@"
