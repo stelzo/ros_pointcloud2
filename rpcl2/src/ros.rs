922d3e226d1f1d86687b4e39d024275abf08180c
stelzo <stelzo@steado.de>
2025-02-07 10:14:54 +0100

    ros2 client jazzy

--- rpcl2/src/ros.rs
+++ rpcl2/src/ros.rs
@@ -36,6 +36,16 @@ pub struct TimeMsg {
     pub nanosec: u32,
 }
 
+#[cfg(feature = "ros2-interfaces-jazzy")]
+impl From<ros2_interfaces_jazzy::builtin_interfaces::msg::Time> for TimeMsg {
+    fn from(time: ros2_interfaces_jazzy::builtin_interfaces::msg::Time) -> Self {
+        Self {
+            sec: time.sec,
+            nanosec: time.nanosec,
+        }
+    }
+}
+
 #[cfg(feature = "rosrust_msg")]
 impl From<rosrust::Time> for TimeMsg {
     fn from(time: rosrust::Time) -> Self {
@@ -55,6 +65,17 @@ pub struct HeaderMsg {
     pub frame_id: String,
 }
 
+#[cfg(feature = "ros2-interfaces-jazzy")]
+impl From<ros2_interfaces_jazzy::std_msgs::msg::Header> for HeaderMsg {
+    fn from(header: ros2_interfaces_jazzy::std_msgs::msg::Header) -> Self {
+        Self {
+            seq: 0,
+            stamp: header.stamp.into(),
+            frame_id: header.frame_id,
+        }
+    }
+}
+
 /// Describing a point encoded in the byte buffer of a PointCloud2 message. See the [official message description](https://docs.ros2.org/latest/api/sensor_msgs/msg/PointField.html) for more information.
 #[derive(Clone, Debug)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
@@ -72,6 +93,89 @@ impl Default for PointFieldMsg {
             offset: 0,
             datatype: 0,
             count: 1,
+        }
+    }
+}
+
+#[cfg(feature = "ros2-interfaces-jazzy")]
+impl From<ros2_interfaces_jazzy::sensor_msgs::msg::PointCloud2> for crate::PointCloud2Msg {
+    fn from(msg: ros2_interfaces_jazzy::sensor_msgs::msg::PointCloud2) -> Self {
+        Self {
+            header: HeaderMsg {
+                seq: 0,
+                stamp: TimeMsg {
+                    sec: msg.header.stamp.sec,
+                    nanosec: msg.header.stamp.nanosec,
+                },
+                frame_id: msg.header.frame_id,
+            },
+            dimensions: crate::CloudDimensions {
+                width: msg.width,
+                height: msg.height,
+            },
+            fields: msg
+                .fields
+                .into_iter()
+                .map(|field| PointFieldMsg {
+                    name: field.name,
+                    offset: field.offset,
+                    datatype: field.datatype,
+                    count: field.count,
+                })
+                .collect(),
+            endian: if msg.is_bigendian {
+                crate::Endian::Big
+            } else {
+                crate::Endian::Little
+            },
+            point_step: msg.point_step,
+            row_step: msg.row_step,
+            data: msg.data,
+            dense: if msg.is_dense {
+                crate::Denseness::Dense
+            } else {
+                crate::Denseness::Sparse
+            },
+        }
+    }
+}
+
+#[cfg(feature = "ros2-interfaces-jazzy")]
+impl From<crate::PointCloud2Msg> for ros2_interfaces_jazzy::sensor_msgs::msg::PointCloud2 {
+    fn from(msg: crate::PointCloud2Msg) -> Self {
+        ros2_interfaces_jazzy::sensor_msgs::msg::PointCloud2 {
+            header: ros2_interfaces_jazzy::std_msgs::msg::Header {
+                stamp: ros2_interfaces_jazzy::builtin_interfaces::msg::Time {
+                    sec: msg.header.stamp.sec,
+                    nanosec: msg.header.stamp.nanosec,
+                },
+                frame_id: msg.header.frame_id,
+            },
+            height: msg.dimensions.height,
+            width: msg.dimensions.width,
+            fields: msg
+                .fields
+                .into_iter()
+                .map(
+                    |field| ros2_interfaces_jazzy::sensor_msgs::msg::PointField {
+                        name: field.name,
+                        offset: field.offset,
+                        datatype: field.datatype,
+                        count: field.count,
+                    },
+                )
+                .collect(),
+            is_bigendian: match msg.endian {
+                crate::Endian::Big => true,
+                crate::Endian::Little => false,
+            },
+            point_step: msg.point_step,
+            row_step: msg.row_step,
+            data: msg.data,
+            is_dense: match msg.dense {
+                crate::Denseness::Dense => true,
+                crate::Denseness::Sparse => false,
+            },
         }
     }
 }
